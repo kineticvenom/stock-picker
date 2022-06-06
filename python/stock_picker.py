9548318249b8a5a1c54cb2f8a0ffbb313201f5dc
kineticvenom <kalebvarnes@yahoo.com>
2022-06-06 11:37:00 -1000

    Python Solution

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,21 @@
 def picker(prices):
-    pass
+    # print(prices)
+    i=0
+    buy_sell = [0,0]
+    biggest_spread = 0
+    for day in range(0,len(prices)) :
+        # print(day)
+        i = day+1
+        for i in range(day+1,len(prices)) :
+           
+            if i < len(prices):
+                # print(prices[i],prices[day])
+                sum = (prices[i]  - prices[day])
+                
+                if biggest_spread < sum:
+                    biggest_spread = sum
+                    buy_sell[0]= day
+                    buy_sell[1]= i
+                # print(sum)
+                i+=1
+    return(buy_sell)
