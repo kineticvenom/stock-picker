f511d530d73bfec36ef81e3558d3b26b2e79b58d
kineticvenom <kalebvarnes@yahoo.com>
2022-06-06 10:34:03 -1000

    JS Solution

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,16 @@
 exports.picker = function(prices) {
-
+    let biggestSpread = 0
+    let buySell = []
+    for (day= 0; day, day < prices.length; day++) {   
+        let sum = 0;
+        for(let i =day+1 ; i < prices.length; i++){ 
+            sum = (prices[i] - prices[day])
+            if (sum > biggestSpread) {
+                biggestSpread = sum;
+                buySell[0] = day;
+                buySell[1] = i;
+            }
+        }
+    }
+    return buySell
 }
